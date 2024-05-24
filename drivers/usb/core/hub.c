f65f9acb738627fc9b7eaa3bfb1a378beb43a08c
Pranav Vashi <neobuddy89@gmail.com>
2024-05-25 01:55:55 +0530

    drivers: usb: Fix unused function warning
    Change-Id: Ib6005c2cda0aaba2a98677aa2174ec6f584881b5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4450,6 +4450,7 @@ static int hub_set_address(struct usb_device *udev, int devnum)
 	return retval;
 }
 
+#ifndef CONFIG_MACH_XIAOMI_SM8150
 /*
  * There are reports of USB 3.0 devices that say they support USB 2.0 Link PM
  * when they're plugged into a USB 2.0 port, but they don't work when LPM is
@@ -4476,6 +4477,7 @@ static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 		usb_enable_usb2_hardware_lpm(udev);
 	}
 }
+#endif
 
 static int hub_enable_device(struct usb_device *udev)
 {
