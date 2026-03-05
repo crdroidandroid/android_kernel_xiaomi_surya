0d6c5979658ab8dea07acd54e00ee9ed61159611
shoya0x00 <shoya0x00@gmail.com>
2026-03-05 23:13:10 +0100

    fixup! BACKPORT: umh: add exit routine for UMH process

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1572,7 +1572,6 @@ extern struct pid *cad_pid;
 #define PF_MEMALLOC		0x00000800	/* Allocating memory */
 #define PF_NPROC_EXCEEDED	0x00001000	/* set_user() noticed that RLIMIT_NPROC was exceeded */
 #define PF_USED_MATH		0x00002000	/* If unset the fpu must be initialized before use */
-#define PF_UMH			0x00004000	/* I'm a Usermodehelper process */
 #define PF_NOFREEZE		0x00008000	/* This thread should not be frozen */
 #define PF_FROZEN		0x00010000	/* Frozen for system suspend */
 #define PF_KSWAPD		0x00020000	/* I am kswapd */
@@ -1584,6 +1583,7 @@ extern struct pid *cad_pid;
 #define PF_SWAPWRITE		0x00800000	/* Allowed to write to swap */
 #define PF_MEMSTALL		0x01000000	/* Stalled due to lack of memory */
 #define PF_PERF_CRITICAL	0x02000000	/* Thread is performance-critical */
+#define PF_UMH			0x02000000	/* I'm a Usermodehelper process */
 #define PF_NO_SETAFFINITY	0x04000000	/* Userland is not allowed to meddle with cpus_allowed */
 #define PF_MCE_EARLY		0x08000000      /* Early kill for mce process policy */
 #define PF_WAKE_UP_IDLE         0x10000000	/* TTWU on an idle CPU */
