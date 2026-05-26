4120ea921ec807e7b1eb765087cf6b114f49a7d3
shoya0x00 <shoya0x00@gmail.com>
2026-05-26 20:48:53 +0200

    Revert "UPSTREAM: SELinux: Abstract use of inode security blob"
    This reverts commit cce6af5c1978d2f1d91c191cab9a06d1f1198446.

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -158,10 +158,4 @@ struct perf_event_security_struct {
 	u32 sid;  /* SID of perf_event obj creator */
 };
 
-static inline struct inode_security_struct *selinux_inode(
-						const struct inode *inode)
-{
-	return inode->i_security;
-}
-
 #endif /* _SELINUX_OBJSEC_H_ */
